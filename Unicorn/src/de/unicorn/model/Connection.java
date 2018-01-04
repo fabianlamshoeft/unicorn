9c04aa82c0bbf48737dfd35333fc15fbe3feffe8
fabian <fabian@fabian-ThinkPad-Yoga-460>
2018-01-04 16:53:04 +0100

    peerServerPort Attribut und Nachricht verhalten
    Hinzufügen eines peerServerPort Attributes, welche den ServerPort des
    Peers beinhaltet. Zudem wurde das Verhalten für eine einkommende
    Nachricht teilweise Implementiert. TODO: Facade anrufen

--- Unicorn/src/de/unicorn/model/Connection.java
+++ Unicorn/src/de/unicorn/model/Connection.java
@@ -5,16 +5,31 @@ import java.util.LinkedList;
 
 public class Connection {
 
-	// Session Name des Kommunikationspartners
+	/**
+	 * Session Name des Kommunikationspartners
+	 */
 	private String name;
-	// Nachrichten Historie. Enthält sowohl entpfangende als auch gesendete Nachrichten
-	// Einträge entsprechen der Art: <Name><Zeitindex><Nachricht>
+	/**
+	 * Portnummer des Serverports vom Kommunikationspartner
+	 */
+	private int peerServerPort;
+	/**
+	 *  Nachrichten Historie. Enthält sowohl entpfangende als auch gesendete Nachrichten
+	 *  Einträge entsprechen der Art: <Name><Zeitindex><Nachricht>
+	 */
+	
 	private LinkedList<String> history;
-	// InputPort für eingehende Nachrichten des Kommunikationspartners
+	/**
+	 *  InputPort für eingehende Nachrichten des Kommunikationspartners
+	 */
 	private InputPort in;
-	// OutputPort zum Senden
+	/**
+	 *  OutputPort zum Senden
+	 */
 	private OutputPort out;
-	// Zeitpunkt des letzten Pokes
+	/**
+	 *  Zeitpunkt des letzten Pokes
+	 */
 	private long pokeTime;
 	
 	
@@ -66,6 +81,17 @@ public class Connection {
 		return pokeTime;
 	}
 	
+	public int getPeerServerPort() {
+		return peerServerPort;
+	}
+
+
+
+	public void setPeerServerPort(int peerServerPort) {
+		this.peerServerPort = peerServerPort;
+	}
+	
+	
 	/**
 	 * Stellt fest um was für eine Nachricht es sich handelt und führt abhängig davon die passenden Operationen aus.
 	 * @param message einkommende Nachricht
@@ -86,12 +112,21 @@ public class Connection {
 			
 		}else if (message.startsWith("MESSAGE")) {
 			
-			String [] pokeArguments = message.split(" ");
+			String [] messageArguments = message.split(" ");
 			// Message Nachricht auf Gültigkeit prüfen. Falls ungültige Nachricht: ignorieren
-			if (pokeArguments.length == 5 && SyntaxChecker.isWellFormedSessionName(pokeArguments[1]) 
-					&& SyntaxChecker.isWellFormedIpAdress(pokeArguments[2]) && SyntaxChecker.isNummeric(pokeArguments[3])) {
+			if (messageArguments.length == 5 && SyntaxChecker.isWellFormedSessionName(messageArguments[1]) 
+					&& SyntaxChecker.isWellFormedIpAdress(messageArguments[2]) && SyntaxChecker.isNummeric(messageArguments[3])) {
 				
-				// TODO: Reagieren
+				// Überprüfen, ob der angegebene Peer in der Pokenachricht der Verbindung entspricht, welche über diesen Port senden darf.
+				// Falls ungültig: Nachricht ignorieren
+				if (messageArguments[1].equals(name) && messageArguments[2].equals(in.getSocket().getInetAddress().getHostAddress())
+						&& Integer.parseInt(messageArguments[3]) == peerServerPort) {
+					
+					// Eintragen in Message History
+					
+					history.add(name + ": " + messageArguments[4]);
+					// TODO: notifyObervers in Facade aufrufen...
+				}
 				
 			}
 		}else if (message.startsWith("DISCONNECT")) {
@@ -107,5 +142,8 @@ public class Connection {
 		}
 		
 	}
+
+
+
 	
 }
