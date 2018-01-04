7d2f90d191fa2c5e5bd0524676a95082f6a6aa4c
fabian <fabian@fabian-ThinkPad-Yoga-460>
2018-01-04 15:39:19 +0100

    Schreiben des InputPorts (noch ungetestet)

--- Unicorn/src/de/unicorn/model/InputPort.java
+++ Unicorn/src/de/unicorn/model/InputPort.java
@@ -1,5 +1,59 @@
 package de.unicorn.model;
 
-public class InputPort {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
+public class InputPort extends Thread{
+
+	private Socket in;
+	private Connection conn;
+	private boolean isRunning = false;
+	
+	public InputPort (Socket in, Connection conn) {
+		this.in = in;
+		this.conn = conn;
+	}
+	
+	public void run() {
+		
+		try {
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in.getInputStream()));
+			String message = "";
+			
+			while (isRunning) {
+				message = reader.readLine();
+				conn.interpretIncommingMessage(message);
+			}
+			
+			reader.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void startListener() {
+		isRunning = true;
+		super.start();
+	}
+	
+	public void close() {
+		isRunning = false;
+		
+		try {
+			super.join();
+			in.close();
+		} catch (InterruptedException e) {
+			// ignore
+		} catch (IOException e) {
+			// ignore
+		}
+		
+	}
+	
 }
