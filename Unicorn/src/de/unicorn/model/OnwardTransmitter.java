e2e1cc2dbb966152410ac4320c4d6cf9e37471a1
Simon <Simon@SIMI-PC.Speedport_W_921V_1_44_000>
2018-01-05 00:01:39 +0100

    Dokumentation OnwardTransmitter erstellt.

--- Unicorn/src/de/unicorn/model/OnwardTransmitter.java
+++ Unicorn/src/de/unicorn/model/OnwardTransmitter.java
@@ -1,5 +1,24 @@
 package de.unicorn.model;
-
+/**
+ * OnwardTransmitter stellt eine Reihe von Methoden zur Verfügung, mit dessen Hilfe 
+ * Poke und Disconnect Nachrichten an alle bekannten Peers weitergeleitet werden kann.
+ * @author fabian
+ *
+ */
 public class OnwardTransmitter {
+    /**
+     * Leitet eine Poke Nachricht an alle bekannten Peers weiter, welche ungleich dem ursprünglichen
+     * Absender der Pokenachricht sowie dem letzten Absender der Pokenachricht sind.
+     * @param conn Verbindung, welche die Pokenachricht gesendet bzw. weitergeleitet hat.
+     * @param pokeName
+     * @param pokeIp
+     * @param pokePort f
+     */
+    public void forwardPushMessage(Connection conn, String pokeName, String pokeIp, int pokePort) {
 
+    }
+
+    public void forwardDeisconnectMessage(Connection conn, String discName, String discIp, int discPort) {
+
+    }
 }
