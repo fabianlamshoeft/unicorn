86d5b0d1117f1e3d2e5e396d047926355df84bcb
Simon <Simon@SIMI-PC.Speedport_W_921V_1_44_000>
2018-01-05 00:14:16 +0100

    ConnectionFactory verändert.

--- Unicorn/src/de/unicorn/model/ConnectionFactory.java
+++ Unicorn/src/de/unicorn/model/ConnectionFactory.java
@@ -1,5 +1,35 @@
 package de.unicorn.model;
 
-public class ConnectionFactory {
+import java.net.Socket;
 
+public class ConnectionFactory {
+	private Connection conn;
+	private Thread thread;
+	private long creationTime;
+	
+	public ConnectionFactory(String ip, int port, Socket in)
+	{
+		
+	}
+	
+	public ConnectionFactory(String name, String ip, int port, Socket in)
+	{
+		
+	}
+	
+	public void create()
+	{
+		
+	}
+	
+	public void create(String name)
+	{
+		
+	}
+	
+	private void startTimer()
+	{
+		
+	}
+	
 }
