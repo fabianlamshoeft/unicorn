4cf0522564232dea0e0832b1598a0064550cdb8c
lenascholz <lenascholz@lenas-mbp>
2018-01-04 23:38:29 +0100

    Ich hab die Ausgabe verändert

--- Unicorn/src/de/unicorn/main/Main.java
+++ Unicorn/src/de/unicorn/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Simon");
+		System.out.println("Hallo Simon und Fabian!");
 	}
 
 }
