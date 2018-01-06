c9d873651e7378aed5318762a03f1f004600880b
fabian <fabian@fabian-ThinkPad-Yoga-460>
2018-01-06 11:16:34 +0100

    Hinzufügen der Methodensignaturen und der obsList in Facade
    TODO: Methoden implementieren

--- Unicorn/src/de/unicorn/model/Facade.java
+++ Unicorn/src/de/unicorn/model/Facade.java
@@ -1,5 +1,52 @@
 package de.unicorn.model;
 
+import java.util.LinkedList;
+
 public class Facade {
+	
+	private static LinkedList<IFacadeObserver> obsList = new LinkedList<IFacadeObserver>();
+	
+	public static void startUp() {
 		
+	}
+	
+	public static void connect(String ip, int port) {
+		
+	}
+	
+	public static void sendMessage(String name, String message) {
+		
+	}
+	
+	public static void sendMessage(String ip, int port, String message) {
+		
+	}
+	
+	public static String[] getPeerList() {
+		return null;
+	}
+	
+	public static LinkedList<String> getMessageHistory(String name, String ip, int port){
+		return null;
+	}
+	
+	public static void disconnect() {
+		
+	}
+	
+	public static void exit() {
+		
+	}
+	
+	public static void register(IFacadeObserver obs) {
+		
+	}
+	
+	public static void unregister(IFacadeObserver obs) {
+		
+	}
+	
+	public static void notifyObservers(){
+		
+	}
 }
