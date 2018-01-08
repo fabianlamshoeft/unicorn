2b6f39063a3802146bb27b1646321d2872cdf5e6
fabian <fabian@fabian-ThinkPad-Yoga-460>
2018-01-08 19:55:23 +0100

    Angefangen, klassen für das Debugging zu schreiben.

---
+++ Unicorn/src/de/unicorn/controller/TerminalController.java
@@ -0,0 +1,95 @@
+package de.unicorn.controller;
+
+import java.util.Scanner;
+
+import de.unicorn.model.Facade;
+import de.unicorn.model.IFacadeObserver;
+
+public class TerminalController extends Thread implements IFacadeObserver{
+	
+	private boolean isRunning = true;
+	
+	public void run() {
+		
+		System.out.println("Willkommen bei Unicorn 1.0!");
+		System.out.println("Bitte geben Sie Ihren Session Namen an:");
+		Scanner s = new Scanner(System.in);
+		
+		String name;
+		String ip;
+		String port;
+		
+		name = s.nextLine();
+		
+		System.out.println("Bitte geben Sie Ihren Port an, unter dem Sie erreichbar sein wollen:");
+		
+		port = s.nextLine();
+		
+		System.out.println("Bitte geben Sie Ihre IP-Adresse an, unter der Sie erreichbar sind:");
+		
+		ip = s.nextLine();
+		
+		System.out.println("------------------------- READY -------------------------");
+		
+		try {
+			Facade.startUp(name, ip, Integer.parseInt(port));
+			
+			while(isRunning) {
+				
+				String entry;
+				
+				entry = s.nextLine();
+				
+				if (entry.startsWith("CONNECT")) {
+					
+					String[] a = entry.split(" ");
+					System.out.println("Sending POKE...");
+					Facade.connect(a[1], Integer.parseInt(a[2]));
+					
+					
+				}else if(entry.startsWith("M")) {
+					
+					String[] a = entry.split(" ");
+					System.out.println("Sending Message...");
+					String message = entry.substring(a[1].length() + 1);
+					Facade.sendMessage(a[1], message);
+					
+				}else if(entry.startsWith("MX")) {
+					
+					String[] a = entry.split(" ");
+					System.out.println("Sending Message...");
+					String message = entry.substring(a[1].length() + a[2].length() + 4);
+					Facade.sendMessage(a[1], Integer.parseInt(a[2]), message);
+					
+					
+				}else if (entry.startsWith("DISCONNECT")) {
+					Facade.disconnect();
+				}else if (entry.startsWith("EXIT")) {
+					Facade.exit();
+					isRunning = false;
+				}
+				
+				
+			}
+			
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Unicorn/src/de/unicorn/testcases/TerminalTest.java
@@ -0,0 +1,11 @@
+package de.unicorn.testcases;
+
+import de.unicorn.controller.TerminalController;
+
+public class TerminalTest {
+	private static TerminalController controller = new TerminalController();
+	public static void main (String[] args) {
+		controller.start();
+	}
+	
+}
