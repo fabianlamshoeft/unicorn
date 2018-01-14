c7a63a0e3127d09dc9908b882b845b296e9dd1ad
lenascholz <lenascholz@lenas-mbp>
2018-01-14 22:09:57 +0100

    Chat DONE

--- Unicorn/src/de/unicorn/view/Chat.java
+++ Unicorn/src/de/unicorn/view/Chat.java
@@ -2,24 +2,18 @@ package de.unicorn.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
-import java.awt.ScrollPane;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.net.InetAddress;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -34,15 +28,8 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
 import de.unicorn.controller.ChatController;
-import de.unicorn.controller.LoginController;
-import de.unicorn.model.Connection;
 import de.unicorn.model.Facade;
-import de.unicorn.view.components.BtnUc;
-/**
- * 
- * @author lenascholz
- *
- */
+
 public class Chat extends JFrame {
 	
 	private ChatController controller;
@@ -50,28 +37,48 @@ public class Chat extends JFrame {
 	private JList<String> nachrichten = new JList<String>();
 	private JList<String> peers = new JList<String>();
 	
-	
+	/**
+	 * <i>nachrichten</i>-Getter für <i>ChatController</i>
+	 * @return nachrichten
+	 */
 	public JList<String> getNachrichten() {
 		return nachrichten;
 	}
-
+	
+	/**
+	 * <i>textFeld</i>-Getter für <i>ChatController</i>
+	 * @return textFeld
+	 */
 	public JTextField getTextFeld() {
 		return textFeld;
 	}
 	
+	/**
+	 * <i>peers</i>-Getter für <i>ChatController</i>
+	 * @return peers
+	 */
 	public JList<String> getPeers() {
 		return peers;
 	}
-
+	
+	/**
+	 * Konstruktor
+	 * @throws HeadlessException
+	 */
 	public Chat() throws HeadlessException {
 		setUpElements();
 		controller = new ChatController(this);
 	}
 	
+	/**
+	 * Wird durch <i>Let's go!</i>-Button in Login bzw. LoginController aufgerufen.
+	 * Das <i>Chat</i>-Fenster öffnet sich. </br>
+	 * Größe und Name von <i>window</i> werden gesetzt.
+	 */
 	public static void newScreen() {
 		Toolkit t = Toolkit.getDefaultToolkit(); 
 		Dimension d = t.getScreenSize(); 
-		int x = (int) (d.getWidth() - 1000) / 2 ;				// d.getWidth() gesamte Bildschirmbreite (Zentrum des Bildschirms berechnen)
+		int x = (int) (d.getWidth() - 1000) / 2 ;				
 		
 		Chat window = new Chat();
 		window.setTitle("Chat - " + Facade.getName() + " - " + Facade.getPort());	
@@ -81,27 +88,36 @@ public class Chat extends JFrame {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 	}
-
+	
+	/**
+	 * In dieser Methode stehen alle Informationen bezüglich des Layouts, der Farbigkeit, 
+	 * der Größe der einzelnen Bausteine, der Schrift etc.
+	 * </br>
+	 * Es folgt die Verknüpfung der Buttons mit ActionListenern, welche mit dem 
+	 * <i>ChatController</i> verknüpft sind, um dort die Funktion zu ergänzen.
+	 */
 	private  void setUpElements() {
-		
 		Container alles = this.getContentPane();
+		
+		// Initialisierung von content + Festlegung von Äußerlichkeiten
 		JPanel content = new JPanel();
 		content.setBackground(Color.decode("0xFFFFFF"));
 		content.setLayout(new BorderLayout());
 		content.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
+		content.getSize(new Dimension(WIDTH, 400));
+		content.setBounds(100, 100, WIDTH, 400);
+		content.setForeground(Color.decode("0x1E647F"));
+		content.setBackground(Color.decode("0xFFFFFF"));
 		
-		//textFeld.setSize(textFeld.getPreferredSize());
-		Color c = Color.decode("0x1E647F");
-		textFeld.setForeground(c);
+		// Festlegung der Äußerlichkeiten von textFeld
+		textFeld.setForeground(Color.decode("0x1E647F"));
 		textFeld.requestFocusInWindow();
 		textFeld.setSize(new Dimension(14000, 38));
 		textFeld.setPreferredSize(new Dimension(14000, 38));
 		textFeld.setMinimumSize(new Dimension(14000, 38));
 		textFeld.setMaximumSize(new Dimension(14000, 38));
 		
-		//content.add(textFeld, BorderLayout.SOUTH); 
-		
-		
+		// Initialisierung der JButtons
 		JButton btnConnect 		= new JButton();
 		JButton btnDisconnect	= new JButton("DISCONNECT");
 		JButton btnExit 			= new JButton("EXIT");
@@ -109,6 +125,7 @@ public class Chat extends JFrame {
 		JButton btnMx 			= new JButton("<html>MX <i>IP</i> <i>Port</i> <i>Text</i></html>");
 		JButton btnOk 			= new JButton("SENDEN");
 		
+		// Festlegung der Äußerlichkeiten von btnConnect
 		btnConnect.setText("<html>CONNECT <i>IP</i> <i>Port</i></html");
 		btnConnect.setSize(new Dimension(170,40));
 		btnConnect.setPreferredSize(new Dimension(170,40));
@@ -119,6 +136,7 @@ public class Chat extends JFrame {
 		btnConnect.setBackground(Color.decode("0xFBE7A8"));
 		btnConnect.setToolTipText("Sende eine POKE-Nachricht mit den eigenen Daten an den unter IP/Port erreichbaren Peer." );
 		
+		// Festlegung der Äußerlichkeiten von btnDisconnect
 		btnDisconnect.setSize(new Dimension(170,40));
 		btnDisconnect.setPreferredSize(new Dimension(170,40));
 		btnDisconnect.setMinimumSize(new Dimension(170,40));
@@ -128,6 +146,7 @@ public class Chat extends JFrame {
 		btnDisconnect.setBackground(Color.decode("0xFBE7A8"));
 		btnDisconnect.setToolTipText("Sende eine DISCONNECT-Nachricht mit den eigenen Daten an alle aktiven Peers und entferne diese aus der Peer-Liste.");
 		
+		// Festlegung der Äußerlichkeiten von btnExit
 		btnExit.setSize(new Dimension(170, 40));
 		btnExit.setPreferredSize(new Dimension(170,40));
 		btnExit.setMinimumSize(new Dimension(170,40));
@@ -137,6 +156,7 @@ public class Chat extends JFrame {
 		btnExit.setBackground(Color.decode("0xFBE7A8"));
 		btnExit.setToolTipText("Sende eine DISCONNECT-Nachricht mit den eigenen Daten an alle bekannten Peers und beende den Client.");
 		
+		// Festlegung der Äußerlichkeiten von btnM
 		btnM.setSize(new Dimension(170, 40));
 		btnM.setPreferredSize(new Dimension(170,40));
 		btnM.setMinimumSize(new Dimension(170,40));
@@ -146,6 +166,7 @@ public class Chat extends JFrame {
 		btnM.setBackground(Color.decode("0xFBE7A8"));
 		btnM.setToolTipText("Sende Text als MESSAGE -Nachricht an alle bekannten Peers, die sich mit Name identizieren.");
 		
+		// Festlegung der Äußerlichkeiten von Mx
 		btnMx.setSize(new Dimension(170,40));
 		btnMx.setPreferredSize(new Dimension(170,40));
 		btnMx.setMinimumSize(new Dimension(170,40));
@@ -155,6 +176,7 @@ public class Chat extends JFrame {
 		btnMx.setBackground(Color.decode("0xFBE7A8"));
 		btnMx.setToolTipText("Sende Text als MESSAGE -Nachricht an den durch IP und Port eindeutig identizierten Peer.");
 		
+		// Festlegung der Äußerlichkeiten von btnOk
 		btnOk.setSize(new Dimension(170,40));
 		btnOk.setPreferredSize(new Dimension(170,40));
 		btnOk.setMinimumSize(new Dimension(170,40));
@@ -163,7 +185,7 @@ public class Chat extends JFrame {
 		btnOk.setForeground(Color.decode("0x1E647F"));
 		btnOk.setBackground(Color.decode("0xFBE7A8"));
 		
-		
+		// Initialisierung von ipAdresse + Festlegung von Äußerlichkeiten
 		JLabel ipAdresse = new JLabel("   Meine IP-Adresse: " + Facade.getIp());
 		ipAdresse.getIconTextGap();
 		ipAdresse.setMinimumSize(new Dimension(WIDTH, 30));
@@ -176,29 +198,38 @@ public class Chat extends JFrame {
 		ipAdresse.setFont(new Font("Arial", Font.PLAIN, 14));
 		ipAdresse.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
 
+		// Festlegung der Äußerlichkeiten von peers 
 		peers.setBackground(Color.decode("0xFFFFFF"));
 		peers.setForeground(Color.decode("0x1E647F"));
 		
+		// Initialisierung von unten + Festlegung von Äußerlichkeiten und Layout
 		JPanel unten = new JPanel();
 		unten.setLayout(new BoxLayout(unten, BoxLayout.X_AXIS));
 		unten.setBackground(Color.decode("0xFFFFFF"));
 		unten.add(textFeld);
 		unten.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
 		
+		// Festlegung von Äußerlichkeiten von nachrichten
 		nachrichten.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		nachrichten.setSelectionForeground(Color.decode("0x1E647F"));
 		nachrichten.setForeground(Color.decode("0x1E647F"));
 		
+		// Initialisierung von chatbereich + Festlegung von Äußerlichkeiten und Layout
 		JPanel chatbereich = new JPanel();
 		chatbereich.setBackground(Color.decode("0xFFFFFF"));
 		chatbereich.setLayout(new BoxLayout(chatbereich, BoxLayout.Y_AXIS));
 		chatbereich.add(Box.createVerticalGlue());
+		
+		// Initialisierung von scroller + Festlegung von Äußerlichkeiten
 		JScrollPane scroller = new JScrollPane(nachrichten);
 		scroller.setMaximumSize(getMaximumSize());
 		scroller.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
+		
+		// scroller und unten werden zu chatbereich (nächst größerer Baustein) hinzufügen, 
 		chatbereich.add(scroller);
 		chatbereich.add(unten);
 		
+		// Initialisierung von rechts + Festlegung von Äußerlichkeiten und Layout + Buttons hinzufügen
 		JPanel rechts = new JPanel();
 		rechts.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
 		rechts.setSize(rechts.getPreferredSize());
@@ -217,65 +248,60 @@ public class Chat extends JFrame {
 		rechts.add(Box.createVerticalGlue());
 		rechts.add(btnOk);
 		
-		content.getSize(new Dimension(WIDTH, 400));
-		content.setBounds(100, 100, WIDTH, 400);
-		content.setForeground(Color.decode("0x1E647F"));
-		content.setBackground(Color.decode("0xFFFFFF"));
-		
-		content.add(ipAdresse, BorderLayout.BEFORE_FIRST_LINE);
+		// Initialisierung von scrollerPeers + Festlegung von Äußerlichkeiten und Layout
 		JScrollPane scrollerPeers = new JScrollPane(peers);
 		scrollerPeers.setBorder(BorderFactory.createLineBorder(Color.decode("0x1E647F"), 1));
 		chatbereich.add(scrollerPeers);
+		
+		// Einfügen der Bausteine in content
+		content.add(ipAdresse, BorderLayout.BEFORE_FIRST_LINE);
 		content.add(scrollerPeers, BorderLayout.WEST);
 		content.add(chatbereich, BorderLayout.CENTER);
 		content.add(rechts, BorderLayout.EAST);
 		
 		alles.add(content);
 		
-		
-		// Befehlsbuttons mit ChatController verknüpfen, um Funktionen im ChatController zu setzen
-		btnConnect.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {		
-				controller.btnConnect();
-			}
-		});
-		
-		btnDisconnect.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {		
-				controller.btnDisconnect();	
-			}
-		});
-		
-		btnExit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {		
-				controller.btnExit();
-			}
-		});
-		
-		btnM.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {		
-				controller.btnM();
-			}
-		});
-		
-		btnMx.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {	
-				controller.btnMx();	
-			}
-		});
-		
-		// Sendenbutton mit ChatController verknüpfen, um Funktionen zu setzen
-		btnOk.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {		
-				controller.btnOk();
-			}
-		});
+		btnConnect.addActionListener(new ActionListener() {		//\
+			@Override											// \
+			public void actionPerformed(ActionEvent arg0) {		//  \
+				controller.btnConnect();							//   \
+			}													//    \
+		});														//     \
+																//\
+		btnDisconnect.addActionListener(new ActionListener() {		// \
+			@Override											//  \
+			public void actionPerformed(ActionEvent arg0) {		//	 \
+				controller.btnDisconnect();						//    \
+			}													//     \
+		});														//\
+																// \
+		btnExit.addActionListener(new ActionListener() {			//  \
+			@Override											//	 \	
+			public void actionPerformed(ActionEvent arg0) {		//	  \	
+				controller.btnExit();							//		
+			}													//		Befehlsbuttons mittelst ActionListeners
+		});														//		mit ChatController verknüpfen, um
+																//		Funktionen im ChatController zu setzen.
+		btnM.addActionListener(new ActionListener() {				//
+			@Override											//	   /
+			public void actionPerformed(ActionEvent arg0) {		//	  /
+				controller.btnM();								//   /
+			}													//  /
+		});														// /
+																///
+		btnMx.addActionListener(new ActionListener() {			//     /
+			@Override											//    /
+			public void actionPerformed(ActionEvent arg0) {		//   /
+				controller.btnMx();								//  /
+			}													// /
+		});														///
+																//      /
+		btnOk.addActionListener(new ActionListener() {			//     /
+			@Override											//    /
+			public void actionPerformed(ActionEvent arg0) {		//   /	
+				controller.btnOk();								//  /
+			}													// /
+		});														///
 					
 		// Bei ENTER-Taste gleiche Aktion wie bei btnOk
 	    KeyListener tfKeyListener = new KeyAdapter() {
@@ -285,7 +311,7 @@ public class Chat extends JFrame {
 	        }
 	    };
 	    
-	    //
+	    // Verbindung zum ChatController herstellen, um exitAndClose() auszuführen
 	    addWindowListener(new WindowAdapter()
         {
             @Override
